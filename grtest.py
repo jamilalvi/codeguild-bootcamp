0eb1344d261dfe43c55eb2ce6a9daaf2919aebe2
jamilalvi <jamil.alvi@yahoo.com>
2015-01-15 13:40:12 -0800

    initial commit

---
+++ grtest.py
@@ -0,0 +1,19 @@
+from graphics import *
+
+def main():
+   win = GraphWin("My Circle", 100, 100)
+   c = Circle(Point(50,50), 10)
+   c.draw(win)
+   
+   while True:
+      s = raw_input("Command: ")
+      if s == 'quit':
+         break
+      elif s == 'move right':
+         c.move(10, 0)
+      elif s == 'move left':
+         c.move(-10, 0)
+      
+   win.close()    
+
+main()
