20a341d9eae6cb3c3ac2fa63e3988c80addda7e1
jamilalvi <jamil.alvi@yahoo.com>
2015-01-22 13:52:39 -0800

    initial commit

---
+++ todolist/todolist.py
@@ -0,0 +1,18 @@
+
+'''
+   This module implements the classes for the todo list application
+'''
+
+class TodoListStorage(object):
+   """Used by TodoList, this class takes care or saving a loading todo lists"""
+   pass
+   
+class TodoList(object):
+   """Manage a list of ToDo items"""
+   pass
+   
+   
+class TodoItem(object):
+   """Manage a single ToDo item"""
+   pass
+   
